17df4de795be5ae3883419a760e79841ed13dbac
aixiao <778730014@qq.com>
2021-11-22 09:48:56 +0800

    '登录模块'

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -1,6 +1,9 @@
 <template>
   <section class="app-main">
-    <transition name="fade-transform" mode="out-in">
+    <transition
+      name="fade-transform"
+      mode="out-in"
+    >
       <router-view :key="key" />
     </transition>
   </section>
@@ -10,7 +13,7 @@
 export default {
   name: 'AppMain',
   computed: {
-    key() {
+    key () {
       return this.$route.path
     }
   }
@@ -25,7 +28,7 @@ export default {
   position: relative;
   overflow: hidden;
 }
-.fixed-header+.app-main {
+.fixed-header + .app-main {
   padding-top: 50px;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
 const path = require('path')
 const defaultSettings = require('./src/settings.js')
 
-function resolve(dir) {
+function resolve (dir) {
   return path.join(__dirname, dir)
 }
 
@@ -16,6 +16,7 @@ const name = defaultSettings.title || 'vue Admin Template' // page title
 const port = process.env.port || process.env.npm_config_port || 9528 // dev port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
+// .............................................
 module.exports = {
   /**
    * You will need to set publicPath if you plan to deploy your site under a sub path,
@@ -36,6 +37,13 @@ module.exports = {
       warnings: false,
       errors: true
     },
+    proxy: {
+      // 当我们的本地的请求 有/api的时候，就会代理我们的请求地址向另外一个服务器发出请求
+      '/api': {
+        target: 'http://ihrm-java.itheima.net/', // 跨域请求的地址
+        changeOrigin: true // 只有这个值为true的情况下 才表示开启跨域
+      }
+    },
     before: require('./mock/mock-server.js')
   },
   configureWebpack: {
@@ -48,7 +56,7 @@ module.exports = {
       }
     }
   },
-  chainWebpack(config) {
+  chainWebpack (config) {
     // it can improve the speed of the first screen, it is recommended to turn on preload
     config.plugin('preload').tap(() => [
       {
@@ -121,3 +129,27 @@ module.exports = {
       )
   }
 }
+
+// ...........
+// ...........................................
+// module.exports = {
+//   devServer: {
+//     // 代理配置
+//     proxy: {
+//       // 这里的api 表示如果我们的请求地址有/api的时候,就出触发代理机制
+//       // localhost:8888/api/abc  => 代理给另一个服务器
+//       // 本地的前端  =》 本地的后端  =》 代理我们向另一个服务器发请求 （行得通）
+//       // 本地的前端  =》 另外一个服务器发请求 （跨域 行不通）
+//       '/api': {
+//         target: 'http://ihrm-java.itheima.net/', // 我们要代理的地址
+
+//         changeOrigin: true, // 是否跨域 需要设置此值为true 才可以让本地服务代理我们发出请求
+//         // 路径重写
+//         pathRewrite: {
+//           // 重新路由  localhost:8888/api/login  => www.baidu.com/api/login
+//           '^/api': '' // 假设我们想把 localhost:8888/api/login 变成www.baidu.com/login 就需要这么做
+//         }
+//       }
+//     }
+//   }
+// }
